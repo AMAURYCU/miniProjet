88b320bc4ca91bfe9f667a132896465057bf7570
Skullriver <alinanov257@gmail.com>
2023-03-03 16:11:51 +0100

    print test

--- src/algorithms/DivideAndConquer.java
+++ src/algorithms/DivideAndConquer.java
@@ -1,19 +1,12 @@
 package algorithms;
 
-import com.sun.source.tree.Tree;
-
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DivideAndConquer {
-    /*Heuristique basée sur une methode diviser pour reigner
-    Nous avons tout d'abbord besoin d'effectuer un préprocessing sur les données: Nous prenons un point A puis nous calculons
-    le chemain vers le point le plus proche (Dijkstra) de A . Cela nous permets de créer N/2 sous arbres de Steiners
-    ____
-    Nous appliquons après un algorithme de fusion d'arbre pour fusioner les sous arbres obtenus après le préprocessing.
-    Ceci nous créera l'arbre de Steiner
-     */
+
     private Graph graph;
     private Path path;
     private List<Edge> subTrees;
@@ -47,9 +40,56 @@ public class DivideAndConquer {
             value.add(nedge);
         }
         subTrees = value;
-         }
+    }
 
     public List<Edge> getSubTrees() {
         return subTrees;
     }
+
+    protected Tree2D getTree()
+    {
+        HashMap<Point, Tree2D> pointToTree = new HashMap<>();
+        for (Edge e : this.subTrees) {
+            Point u = e.getStart();
+            Point v = e.getEnd();
+
+            if (!pointToTree.containsKey(u)) {
+                pointToTree.put(u, new Tree2D(u, new ArrayList<>()));
+            }
+
+            if (!pointToTree.containsKey(v)) {
+                pointToTree.put(v, new Tree2D(v, new ArrayList<>()));
+            }
+
+            pointToTree.get(u).getSubTrees().add(pointToTree.get(v));
+            pointToTree.get(v).getSubTrees().add(pointToTree.get(u));
+        }
+//
+//        for (Point name: pointToTree.keySet()) {
+//            String key = name.toString();
+//            String value = pointToTree.get(name).getSubTrees().toString();
+//            System.out.println(key + " " + value);
+//        }
+
+        // Build the tree recursively
+        Tree2D steinerTree = buildSubtree(pointToTree.get(this.subTrees.get(0).getStart()), null);
+
+        return null;
+    }
+
+    private static Tree2D buildSubtree(Tree2D currentNode, Tree2D parentNode) {
+        // Remove the parent node from the current node's subtrees
+        if (parentNode != null) {
+            currentNode.getSubTrees().remove(parentNode);
+        }
+
+        // Recursively build subtrees
+        ArrayList<Tree2D> subtrees = new ArrayList<>();
+        for (Tree2D child : currentNode.getSubTrees()) {
+            subtrees.add(buildSubtree(child, currentNode));
+        }
+
+        return new Tree2D(currentNode.getRoot(), subtrees);
+    }
+
 }
